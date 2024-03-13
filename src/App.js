781807890d0757203160399faca4e0003b9c078e
fguedes10 <fadg100@gmail.com>
2024-03-13 19:04:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./pages/home/index.js";
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import PetsIcon from '@mui/icons-material/Pets';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  return(
+  <BrowserRouter>
+  <header>
+  <NavLink>
+      
+      <Button href="/home" variant="contained" startIcon={<HomeIcon/>}>Home</Button>
+      <Button href="/home" variant="contained" startIcon={<PetsIcon/>}>Search Cats</Button>
+    
+  </NavLink>
+</header>
+<Routes>
+  <Route path="/home"  element={<Home />} />
+</Routes>
+</BrowserRouter>
+);
 }
 
 export default App;
---
+++ src/pages/home/index.js
@@ -0,0 +1,49 @@
+import  {React, useEffect, useState } from "react";
+import App from "../../App";
+
+const Home = () => {
+const [catData, setCatData] = useState([]);
+const url = process.env.REACT_APP_API_URL;
+
+const route = url + "v1/images/search/?limit=5";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+
+
+
+    const getAllCats = () => {
+        const options = {
+            method: "GET",
+            headers: {
+                "x-api-key": apiKey,
+                "Content-Type": "application/json",
+            },
+        };
+        fetch(route, options)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Bad request");
+                }
+                return response.json();
+            })
+            .then((data) =>
+            { setCatData(data) 
+             console.log(data)
+             console.log(apiKey)}
+             
+             )
+            .catch((error) => console.error("Error fetching cats: ", error));
+
+           
+    }
+
+    return (
+        <div>
+            <p>Random cat</p>
+            <img src={catData[0] !== undefined && catData[0].url} alt="a cat image" />
+            <button onClick={getAllCats}>Get cat</button>
+        </div>
+    );
+};
+export default Home

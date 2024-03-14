db22608ba734bea1f63a63c5b1f5268873243199
Fguedes10 <fadg100@gmail.com>
2024-03-14 17:37:41 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import Home from "./pages/home/index.js";
+import Home from "./pages/home";
+import SearchCats from "./pages/SearchCats";
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
 import PetsIcon from '@mui/icons-material/Pets';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function App() {
 
 
   return(
   <BrowserRouter>
-  <header>
-  <NavLink>
-      
-      <Button href="/home" variant="contained" startIcon={<HomeIcon/>}>Home</Button>
-      <Button href="/home" variant="contained" startIcon={<PetsIcon/>}>Search Cats</Button>
-    
-  </NavLink>
+  <header className="App-header">
+  <nav className="nav-links">
+    <div >
+      <img className="logo" src="images/logo.png" alt="a cat image" />
+    </div>
+    <div>
+      <div className="buttonsNav">
+      <Button sx={{backgroundColor:"white", color:"black" }} href="/home" variant="contained" startIcon={<HomeIcon/>}>Home</Button>
+      <Button href="/searchCats" variant="contained" startIcon={<PetsIcon/>}>Search Cats</Button>
+      <Button href="/searchCats" variant="contained" startIcon={<FavoriteIcon/>}>Favorites</Button>
+      </div>
+    </div>
+  </nav>
 </header>
 <Routes>
   <Route path="/home"  element={<Home />} />
+  <Route path="/searchCats"  element={<SearchCats />} />
 </Routes>
 </BrowserRouter>
 );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,48 +1,15 @@
-import  {React, useEffect, useState } from "react";
-import App from "../../App";
+import {React} from "react";
 
 const Home = () => {
-const [catData, setCatData] = useState([]);
-const url = process.env.REACT_APP_API_URL;
-
-const route = url + "v1/images/search/?limit=5";
-
-const apiKey = process.env.REACT_APP_API_KEY;
-
-
-
-
-    const getAllCats = () => {
-        const options = {
-            method: "GET",
-            headers: {
-                "x-api-key": apiKey,
-                "Content-Type": "application/json",
-            },
-        };
-        fetch(route, options)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Bad request");
-                }
-                return response.json();
-            })
-            .then((data) =>
-            { setCatData(data) 
-             console.log(data)
-             console.log(apiKey)}
-             
-             )
-            .catch((error) => console.error("Error fetching cats: ", error));
-
-           
-    }
 
     return (
         <div>
-            <p>Random cat</p>
-            <img src={catData[0] !== undefined && catData[0].url} alt="a cat image" />
-            <button onClick={getAllCats}>Get cat</button>
+            <div>
+            <h1>WELCOME CAT LOVER</h1>
+            </div>
+            <div>
+                <img src="images/catimagehome.jpeg" alt="a cat image" />
+            </div>
         </div>
     );
 };

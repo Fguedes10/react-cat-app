97d365a34b8f2ae3bf2693ee253cf42c02873eef
Fguedes10 <fadg100@gmail.com>
2024-03-20 09:25:14 +0000

    fix navbuttons

--- src/components/navButtons.jsx
+++ src/components/navButtons.jsx
@@ -4,13 +4,21 @@ import PetsIcon from "@mui/icons-material/Pets";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ButtonComponent from "./ButtonComponent";
 import UserNameContext from "../components/UserNameContext";
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useNavigate } from "react-router-dom";
 
 const NavButtons = () => {
-  const { userName } = useContext(UserNameContext);
+  const navigate = useNavigate();
+  const { userName, handleNameSubmit } = useContext(UserNameContext);
 
   if (!userName || userName === "") {
     return null;
   }
+
+  const Logout = () => {
+    handleNameSubmit("");
+    navigate("/");
+  };
 
   return (
     <div className="buttonsNav">
@@ -22,12 +30,17 @@ const NavButtons = () => {
       <ButtonComponent
         href="/searchCats"
         startIcon={<PetsIcon />}
-        buttonText="Search Cats"
+        buttonText="Cats"
       />
       <ButtonComponent
         href="/favorites"
         startIcon={<FavoriteIcon sx={{ color: "red" }} />}
         buttonText="Favorites"
+      />
+      <ButtonComponent
+        onClick={() => Logout()}
+        startIcon={<LogoutIcon sx={{ color: "black" }} />}
+        buttonText="logout"
       />
     </div>
   );
